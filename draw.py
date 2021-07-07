bcb253c63ca74906d2fe25b1adc234df1ce60c5e
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-07-07 20:27:29 +0100

    draw text

--- draw.py
+++ draw.py
@@ -3,7 +3,7 @@ import numpy as np
 blank = np.zeros((500,500,3), dtype='uint8')
 cv.imshow('Blank', blank)
 
-# draw rectangle
+"""# draw rectangle
 cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
 cv.imshow('Rectangle', blank)
 
@@ -16,9 +16,9 @@ cv.imshow('Circle', blank)
 cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
 cv.imshow('Line', blank)
 
-
+"""
 # write text
-cv.putText(blank, 'Hello my name is ayoub', (0,255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
+cv.putText(blank, 'Hello my name is ayoub!!!', (0,255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
 cv.imshow('Text', blank)
 
 
