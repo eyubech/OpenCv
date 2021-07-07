4f9c375bd3d72c6f58762d77f60af8d99a1af4ee
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-07-07 20:55:46 +0100

    Canny Edges

--- basic.py
+++ basic.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-img = cv.imread('Photos/cats 2.jpg')
+img = cv.imread('Photos/park.jpg')
 cv.imshow('Cat', img)
 
 # Converting to grayscale
@@ -7,10 +7,11 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 # Blur
-blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
 
-
-
+# Edge Cascade
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny Edges', canny)
 
 cv.waitKey(0)
