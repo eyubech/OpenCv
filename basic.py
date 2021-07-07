7c20ec731219d45adf273f6b8594a118ba42e20e
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-07-07 20:52:34 +0100

    Blur image

--- basic.py
+++ basic.py
@@ -1,11 +1,14 @@
 import cv2 as cv
-img = cv.imread('Photos/cat.jpg')
+img = cv.imread('Photos/cats 2.jpg')
 cv.imshow('Cat', img)
+
 # Converting to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
-
+# Blur
+blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
 
 
 
