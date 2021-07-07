24a287f7159d09498dca5beb8111f4a3629f3c92
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-07-07 20:15:23 +0100

    draw circle and rectangle

--- draw.py
+++ draw.py
@@ -1,9 +1,15 @@
 import cv2 as cv
 import numpy as np
+blank = np.zeros((500,500,3), dtype='uint8')
+cv.imshow('Blank', blank)
+
+#draw rectangle
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
+cv.imshow('Rectangle', blank)
 
 
-img = cv.imread('Photos/cat.jpg')
-cv.imshow('Cat', img)
-
+#draw circle
+cv.circle(blank,(blank.shape[1]//2, blank.shape[0]//2),40, (0,0,255), thickness=3)
+cv.imshow('Circle', blank)
 
 cv.waitKey(0)
