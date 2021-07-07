4a6aa389191edddf61ff2b39d02fdbe19fe4bc56
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-07-07 17:33:58 +0100

    resize live video

--- read.py
+++ read.py
@@ -1,13 +1,16 @@
 import cv2 as cv
-img = cv.imread('Photos/cat.jpg')
-cv.imshow('Cat', img)
+
 def rescaleFrame(frame, scale=0.75):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dimensions = (width, height)
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
-resized_image = rescaleFrame(img)
-cv.imshow('Image', resized_image)
+
+def changeRes(width,height):
+    # live video
+    capture.set(3,width)
+    capture.set(4,height)
+
 
 capture = cv.VideoCapture('Videos/dog.mp4')
 while True:
