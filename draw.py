4690ab6c071a4d5fd541a4996945a1274d9722fd
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-07-07 20:18:54 +0100

    draw line

--- draw.py
+++ draw.py
@@ -9,7 +9,12 @@ cv.imshow('Rectangle', blank)
 
 
 #draw circle
-cv.circle(blank,(blank.shape[1]//2, blank.shape[0]//2),40, (0,0,255), thickness=3)
+cv.circle(blank,(blank.shape[1]//2, blank.shape[0]//2),40, (0,0,255), thickness=-1)
 cv.imshow('Circle', blank)
+
+#draw line
+cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255), thickness=3)
+cv.imshow('Line', blank)
+
 
 cv.waitKey(0)
